df152e5ebacf1cb7d797d46caae1fb0d57b4347b
ramakrishnadas <rama.k.d.b.p@gmail.com>
2024-05-03 13:30:17 -0600

    adapted swagger json to the deployed website

--- swagger.js
+++ swagger.js
@@ -5,11 +5,11 @@ const doc = {
     title: 'Contacts API',
     description: 'This API contains contact information'
   },
-  host: ''
+  host: 'contacts-project-api.onrender.com'
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./server.js'];
+const routes = ['./routes/contacts.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */

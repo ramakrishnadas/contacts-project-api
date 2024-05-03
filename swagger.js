f1d89b367590b0f240dad14af1f3dc15ea88e4a4
ramakrishnadas <rama.k.d.b.p@gmail.com>
2024-05-03 13:35:03 -0600

    adjusted swagger json file to deployed website

--- swagger.js
+++ swagger.js
@@ -3,13 +3,13 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Contacts API',
-    description: 'This API contains contact information'
+    description: 'Get, create, update, and delete contact information.'
   },
   host: 'contacts-project-api.onrender.com'
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./routes/contacts.js'];
+const routes = ['./server.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
